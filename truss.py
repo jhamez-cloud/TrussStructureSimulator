32a653d018c99a88612ef771aa713966a292f4d3
James Kekeli <jameskekeli12@gmail.com>
2026-03-12 21:52:12 +0000

    feature:truss container class component for truss system

--- truss.py
+++ truss.py
@@ -1,11 +1,14 @@
+'''Docstring for truss container class'''
 class Truss:
-
+    '''Doctring for creating container class'''
     def __init__(self):
         self.joints = {}
         self.members = {}
 
     def add_joint(self, joint):
-        self.joints[joint.name] = joint
+        '''Docstring for adding joints'''
+        self.joints[joint.name] = joint #function to add or include joints in the truss container
 
     def add_member(self, member):
-        self.members[member.name] = member
+        '''Docstring for adding members'''
+        self.members[member.name] = member #function to add or include members in the truss container
