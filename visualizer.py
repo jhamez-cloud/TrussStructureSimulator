7017ef1786d72e9277cda2c9929877a4a1441147
James Kekeli <jameskekeli12@gmail.com>
2026-03-12 21:11:19 +0000

    feature:visual graph for truss system

---
+++ visualizer.py
@@ -0,0 +1,25 @@
+import matplotlib.pyplot as plt
+
+class TrussVisualizer:
+
+    def __init__(self, truss):
+        self.truss = truss
+
+    def draw(self):
+
+        for member in self.truss.members.values():
+
+            x = [member.joint1.x, member.joint2.x]
+            y = [member.joint1.y, member.joint2.y]
+
+            plt.plot(x, y, 'b-o')
+
+            midx = (x[0]+x[1])/2
+            midy = (y[0]+y[1])/2
+
+            if member.force is not None:
+                plt.text(midx, midy, f"{member.force:.1f}")
+
+        plt.title("Truss Structure")
+        plt.axis("equal")
+        plt.show()
