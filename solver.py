8ad1b2809dc37bd80fb9a69e6a28f03d4f82a8ee
James Kekeli <jameskekeli12@gmail.com>
2026-03-12 20:54:11 +0000

    feature:calculations for truss system

---
+++ solver.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+class TrussSolver:
+
+    def __init__(self, truss):
+        self.truss = truss
+
+    def solve(self):
+
+        members = list(self.truss.members.values())
+        joints = list(self.truss.joints.values())
+
+        n_members = len(members)
+        n_joints = len(joints)
+
+        A = np.zeros((2*n_joints, n_members))
+        b = np.zeros(2*n_joints)
+
+        joint_index = {joint.name:i for i,joint in enumerate(joints)}
+
+        for m_index, member in enumerate(members):
+
+            c, s = member.direction_cosines()
+
+            j1 = joint_index[member.joint1.name]
+            j2 = joint_index[member.joint2.name]
+
+            A[2*j1][m_index] = c
+            A[2*j1+1][m_index] = s
+
+            A[2*j2][m_index] = -c
+            A[2*j2+1][m_index] = -s
+
+
+        for joint in joints:
+
+            j = joint_index[joint.name]
+
+            b[2*j] = -joint.fx
+            b[2*j+1] = -joint.fy
+
+
+        forces = np.linalg.lstsq(A, b, rcond=None)[0]
+
+        for i, member in enumerate(members):
+            member.force = forces[i]
+
+        return members
